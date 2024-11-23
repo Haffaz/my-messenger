22a0d0428c3633b86b580685240ea0fe02dcee26
Haffaz <haffaz.se@gmail.com>
2024-11-23 14:07:56 +0100

    configure graphql

--- apps/messenger-api/src/index.ts
+++ apps/messenger-api/src/index.ts
@@ -1,9 +1,35 @@
-import { Hono } from 'hono'
+import { type RootResolver, graphqlServer } from "@hono/graphql-server";
+import { buildSchema } from "graphql";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const schema = buildSchema(`
+  type Query {
+    hello: String!
+  }
+`);
 
-export default app
+const rootResolver: RootResolver = (_) => {
+	return {
+		hello: () => "Hello world!",
+	};
+};
+
+app.use(
+	"/graphql",
+	graphqlServer({
+		schema,
+		rootResolver,
+		graphiql: true,
+	}),
+);
+
+const port = process.env.PORT || 3000;
+
+console.log(`Hono 🥟 GraphQL Server Listening on port ${port}`);
+
+export default {
+	port,
+	fetch: app.fetch,
+};
