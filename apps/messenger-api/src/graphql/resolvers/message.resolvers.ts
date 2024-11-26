0b5705269807ef5e3b5602e260e15c315a8b9117
Haffaz <haffaz.se@gmail.com>
2024-11-26 13:17:41 +0100

    fix new message issue

--- apps/messenger-api/src/graphql/resolvers/message.resolvers.ts
+++ apps/messenger-api/src/graphql/resolvers/message.resolvers.ts
@@ -1,6 +1,9 @@
+import { Message } from "@prisma/client";
 import { sendMessageInputSchema } from "@repo/shared-types";
 import { GraphQLError } from "graphql";
+import { withFilter } from "graphql-subscriptions";
 import { convertDateToString } from "../../utils/date";
+import { Thread } from "./codegen-types";
 import { MyResolvers } from "./derived-types";
 
 export const messageResolvers: MyResolvers = {
@@ -22,30 +25,54 @@ export const messageResolvers: MyResolvers = {
       try {
         const validatedInput = sendMessageInputSchema.parse(input);
         return prisma.$transaction(async (tx) => {
-          let thread = await tx.thread.findFirst({
-            where: { id: validatedInput.threadId },
-          });
+          let thread: Pick<Thread, "id"> | null = null;
 
-          // If thread doesn't exist, create a new one
-          if (!thread) {
+          if (validatedInput.threadId) {
+            thread = await tx.thread.findUnique({
+              where: { id: validatedInput.threadId },
+              select: {
+                id: true,
+              },
+            });
+
+            if (!thread) {
+              throw new GraphQLError("Thread not found", {
+                extensions: { code: "NOT_FOUND" },
+              });
+            }
+          } else {
             const receiver = await tx.user.findUnique({
               where: { username: validatedInput.receiverUsername },
             });
 
             if (!receiver) {
               throw new GraphQLError("Receiver not found", {
-                extensions: { code: "BAD_REQUEST" },
+                extensions: { code: "NOT_FOUND" },
               });
             }
 
-            thread = await tx.thread.create({
-              data: {
-                createdById: user.id,
-                participants: {
-                  connect: [{ id: user.id }, { id: receiver.id }],
-                },
+            const existingThread = await tx.thread.findFirst({
+              where: {
+                AND: [
+                  { participants: { some: { id: user.id } } },
+                  { participants: { some: { id: receiver.id } } },
+                ],
+              },
+              select: {
+                id: true,
               },
             });
+
+            thread =
+              existingThread ||
+              (await tx.thread.create({
+                data: {
+                  createdById: user.id,
+                  participants: {
+                    connect: [{ id: user.id }, { id: receiver.id }],
+                  },
+                },
+              }));
           }
 
           const message = await tx.message
@@ -68,18 +95,26 @@ export const messageResolvers: MyResolvers = {
           return message;
         });
       } catch (error) {
-        console.log(error);
-        throw new GraphQLError("Failed to send message", {
-          extensions: { code: "BAD_REQUEST" },
-        });
+        if (error instanceof GraphQLError) throw error;
+
+        throw new GraphQLError(
+          error instanceof Error ? error.message : "Failed to send message",
+          { extensions: { code: "BAD_REQUEST" } },
+        );
       }
     },
   },
   Subscription: {
     messageCreated: {
-      subscribe: (_, { threadId }, { pubsub }) => {
-        return pubsub.asyncIterableIterator([`MESSAGE_CREATED.${threadId}`]);
-      },
+      subscribe: withFilter(
+        (_, { threadId }, { pubsub }) =>
+          pubsub.asyncIterableIterator(`MESSAGE_CREATED.${threadId}`),
+        (payload: { messageCreated: Message }, variables) => {
+          console.log("payload", payload);
+          console.log("variables", variables);
+          return payload.messageCreated.threadId === variables.threadId;
+        },
+      ),
     },
   },
 };
